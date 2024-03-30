a775e69c60b779dca3f09fa9964ef4723560f303
narogishin <tdoctor109@gmail.com>
2024-03-30 19:58:20 +0000

    modified:   player.py 	modified:   settings.py

--- player.py
+++ player.py
@@ -10,8 +10,9 @@ class Player:
     self.color = colors[randint(0, len(colors)-1)]
     self.players_list = []
     self.x, self.y = 25, 25
-    self.n, self.m = 0, 0
+    self.current_index = 0
     self.size = 20
+    self.alpha = True
 
   def check_screen_collision(self, dx, dy):
     # solved by chat gpt
@@ -25,28 +26,50 @@ class Player:
     if (x, y) != setpoint:
       if x < 550 - 25 and y == 25:
         dx += 1
-      if x == 550 - 25 and y <= 500 - 25:
+      if x == 550 - 25 and y <= 550 - 25:
         dy += 1
-      if y == 500 - 25:
+      if y == 550 - 25:
         dx -= 1
-      if x == 25 and y <= 500 - 25:
+      if x == 25 and y <= 550 - 25:
         dy -= 1
       self.check_screen_collision(dx, dy)
 
   def move(self, x, y):
     self.mouvement(self.x, self.y, (x, y))
 
+  def check_right_click(self, index: int):
+    right_click_triggered = False
+    for event in pg.event.get():
+      if event.type == pg.MOUSEBUTTONDOWN:
+        if event.button == 3 and not right_click_triggered:  # Check for right mouse button click
+          self.current_index += index
+          self.current_index %= 40
+          n, m = positions[self.current_index]
+          while (self.x != 25 + 50*n) and (self.y != 25 + 50*m):
+            print("current index", self.current_index)
+            # print("n and m : ", n, m)
+            # print("x and y : ", self.x, self.y)
+            self.move(25 + 50*n, 25 + 50*m)
+          right_click_triggered = True
+      elif event.type == pg.MOUSEBUTTONUP:
+        if event.button == 3:
+          right_click_triggered = False
+
   def play(self):
     click = pg.mouse.get_pressed()[0]
+    index = randint(1, 6)
+    print("index : ", index)
+    # self.check_right_click(index)
     if click:
-      n, m = randint(0, 10), randint(0, 10)
-      if n == 0 or n == 10:
-        m = randint(0, 10)
-      elif m == 0 or m == 9:
-        n = randint(0, 10)
-      # time.sleep(1)
-      print(n, m)
+      self.current_index += index
+      self.current_index %= 40
+      n, m = positions[self.current_index]
+      # while (self.x != 25 + 50*n) and (self.y != 25 + 50*m):
+      print("current index : ", self.current_index)
+      print("n and m : ", n, m)
+      # print("x and y : ", self.x, self.y)
       self.move(25 + 50*n, 25 + 50*m)
+      self.alpha = not self.alpha
 
   def update(self):
     self.play()
--- settings.py
+++ settings.py
@@ -2,10 +2,18 @@
 RATIO = 0.5
 RESOLUTION = WIDTH, HEIGHT = 1100*RATIO, 1100*RATIO
 
-# needed to differentiate between players , gonna use 1 example and to change later
+# needed to differentiate between players, gonna use 1 example and to change later
 names = [chr(65+i)+str(j) for i in range(20) for j in range(1,100)]
 colors = ['red', 'green', 'blue', 'cyan', 'yellow', 'magenta', 'white']
-f = lambda txt, idx: txt.split(',')[idx]
+positions = {
+  0: (0,0), 1: (1,0), 2: (2,0), 3: (3,0), 4: (4,0), 5: (5,0), 6: (6,0),
+  7: (7,0), 8: (8,0), 9: (9,0), 10: (10,0), 11: (10,1), 12: (10,2), 13: (10,3), 14: (10,4),
+   15: (10,5), 16: (10,6), 17: (10,7), 18: (10,8), 19: (10,9), 20: (10,10), 21: (9,10),
+     22: (8,10), 23: (7,10), 24: (6,10), 25: (5,10), 26: (4,10), 27: (3,10), 28: (2,10),
+       29: (1,10), 30: (0,10), 31: (0,9), 32: (0,8), 33: (0,7), 34: (0,6), 35: (0,5),
+         36: (0,4), 37: (0,3), 38: (0,2), 39: (0,1)
+         }
+# f = lambda txt, idx: txt.split(',')[idx]
 
 # SOCKETS
 SERVER_IP = "192.168.1.16"
